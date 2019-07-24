3fa5a3612520d3aef095acd7bd5d11721e36c539
Appleby43 <alexappleby70@gmail.com>
2019-07-24 13:28:39 -0600

    Re-instituted the basic ForwardMotion, now known as ForwardMotionBasic

--- TeamCode/src/main/java/edu/ahs/robotics/MecanumChassis.java
+++ TeamCode/src/main/java/edu/ahs/robotics/MecanumChassis.java
@@ -13,7 +13,11 @@ public class MecanumChassis extends Chassis {
 
     public void execute(PlanElement planElement){
         if (planElement instanceof ForwardMotion) {
-            motionInterpreter((ForwardMotion)planElement);
+            if(planElement instanceof ForwardMotionBasic){
+                motionInterpreter((ForwardMotionBasic)planElement);
+            } else {
+                motionInterpreter((ForwardMotion) planElement);
+            }
         }
         else if (planElement instanceof ArcMotion){
             motionInterpreter((ArcMotion)planElement);
@@ -87,6 +91,22 @@ public class MecanumChassis extends Chassis {
             FTCUtilities.OpLogger("Elapsed Time", currentTime);
             FTCUtilities.OpLogger("-------------", ":-----------");
             
+        }
+
+        setPowerAll(0);
+    }
+
+    private void motionInterpreter(ForwardMotionBasic forwardMotionBasic){
+        double distance = forwardMotionBasic.travelDistance;
+        double power = forwardMotionBasic.motorPower;
+        double averageDistance = 0;
+
+        //reset all encoders?
+
+        setPowerAll(power);
+
+        while (averageDistance < distance) {
+            averageDistance = (backLeft.getDistance() + backRight.getDistance() + frontRight.getDistance() + frontLeft.getDistance())/4;
         }
 
         setPowerAll(0);
