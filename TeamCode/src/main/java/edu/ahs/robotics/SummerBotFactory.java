299ae3a88bb706824c653725a33a4a8ffa0648a2
Appleby43 <alexappleby70@gmail.com>
2019-07-25 16:26:26 -0600

    Finalized motor enums

--- TeamCode/src/main/java/edu/ahs/robotics/SummerBotFactory.java
+++ TeamCode/src/main/java/edu/ahs/robotics/SummerBotFactory.java
@@ -7,8 +7,18 @@ public class SummerBotFactory extends BotFactory {
 
     @Override
     public Robot createRobot() {
+        MecanumChassis mecanumChassis = makeChassis();
+        Robot robot = new Robot(mecanumChassis);
+        return robot;
+    }
+
+    private MecanumChassis makeChassis() {
+        //Set Gear Ratio
         GearRatio driveGearRatio = new GearRatio(1,2);
+        //Set Wheel Diameter in inches and Motor Type. These traits are shared by all chassis drive units
         DriveUnit.Config config = new DriveUnit.Config(driveGearRatio, 3.94, MotorHashService.MotorTypes.YJ_435);
+
+        //Make a HashMap that maps motors to their flip status. True indicates the motor runs reverse.
         Map<ChassisMotors.Mecanum, Boolean> driveFlips  = new HashMap<>();
 
         driveFlips.put(ChassisMotors.Mecanum.FRONTLEFT, false);
@@ -16,8 +26,6 @@ public class SummerBotFactory extends BotFactory {
         driveFlips.put(ChassisMotors.Mecanum.BACKLEFT, false);
         driveFlips.put(ChassisMotors.Mecanum.BACKRIGHT, true);
 
-        MecanumChassis mecanumChassis = new MecanumChassis(config, driveFlips);
-        Robot robot = new Robot(mecanumChassis);
-        return robot;
+        return new MecanumChassis(config, driveFlips);
     }
 }
