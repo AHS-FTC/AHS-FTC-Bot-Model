199a02f98a2b00e4814caf0972c9b515e506146e
Appleby43 <alexappleby70@gmail.com>
2019-07-25 15:44:50 -0600

    Refactored MotorLocations Enum

--- TeamCode/src/main/java/edu/ahs/robotics/MotorLocations.java
+++
@@ -1,5 +0,0 @@
-package edu.ahs.robotics;
-
-public enum MotorLocations{
-    FRONTLEFT, FRONTRIGHT, BACKLEFT, BACKRIGHT, MIDLEFT, MIDRIGHT, LEFT, RIGHT
-}
--- TeamCode/src/main/java/edu/ahs/robotics/SummerBotFactory.java
+++ TeamCode/src/main/java/edu/ahs/robotics/SummerBotFactory.java
@@ -1,12 +1,22 @@
 package edu.ahs.robotics;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SummerBotFactory extends BotFactory {
 
     @Override
     public Robot createRobot() {
         GearRatio driveGearRatio = new GearRatio(1,2);
         DriveUnit.Config config = new DriveUnit.Config(driveGearRatio, 3.94, MotorHashService.MotorTypes.YJ_435);
-        MecanumChassis mecanumChassis = new MecanumChassis(config);
+        Map<ChassisMotors, Boolean> driveFlips  = new HashMap<>();
+
+        driveFlips.put(ChassisMotors.FRONTLEFT, false);
+        driveFlips.put(ChassisMotors.FRONTRIGHT, true);
+        driveFlips.put(ChassisMotors.BACKLEFT, false);
+        driveFlips.put(ChassisMotors.BACKRIGHT, true);
+
+        MecanumChassis mecanumChassis = new MecanumChassis(config, driveFlips);
         Robot robot = new Robot(mecanumChassis);
         return robot;
     }
