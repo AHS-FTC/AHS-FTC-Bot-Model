6d7bae7f770e5eae28112c4df3792c17d45ec65f
Appleby43 <alexappleby70@gmail.com>
2019-12-30 14:54:53 -0700

    Added PointPID and tests

---
+++ TeamCode/src/main/java/edu/ahs/robotics/control/pid/PointPID.java
@@ -0,0 +1,62 @@
+package edu.ahs.robotics.control.pid;
+
+import edu.ahs.robotics.control.Point;
+
+/**
+ * PID class for correcting between two 2d cartesian points on the field.
+ * Based on the simple PID class.
+ * @see PID
+ * @author Alex Appleby
+ */
+public class PointPID {
+    private PID xPID, yPID;
+
+    /**
+     * Full constructor that takes constants for both x and y.
+     * @param xP Scalar for proportional x correction.
+     * @param xI Scalar for x integral correction. Generally much smaller than xP.
+     * @param xD Scalar for x derivative correction. Generally non-negative in this case.
+     * @param yP Scalar for proportional y correction.
+     * @param yI Scalar for y integral correction. Generally much smaller than yP.
+     * @param yD Scalar for y derivative correction. Generally non-negative in this case.
+     */
+    public PointPID(double xP, double xI, double xD, double yP, double yI, double yD) {
+        xPID = new PID(xP,xI,xD);
+        yPID = new PID(yP,yI,yD);
+    }
+
+    /**
+     * Simple constructor where P, I, D, constants are the same for x and y.
+     * @param p Scalar for proportional correction.
+     * @param i Scalar for integral correction. Generally much smaller than p.
+     * @param d Scalar for derivative correction. Generally non-negative in this case.
+     */
+    public PointPID(double p, double i, double d){
+        this(p,i,d,p,i,d);
+    }
+
+    /**
+     * Gets PID corrections, also storing values for proportional and derivative math.
+     * @return Correction class containing corrections in two dimensions.
+     */
+    public Correction getCorrection(Point current, Point target){
+        double xCorrection = xPID.getCorrection(current.x, target.x);
+        double yCorrection = yPID.getCorrection(current.y, target.y);
+
+        return new Correction(xCorrection, yCorrection);
+    }
+
+    /**
+     * Contains PointPID corrections in the domain of x and y in one package.
+     * Only creatable inside the PointPID Class.
+     */
+    public class Correction{
+        public double x,y;
+
+        private Correction(double x, double y){ //only PointPID can create corrections
+            this.x = x;
+            this.y = y;
+        }
+
+    }
+}
---
+++ TeamCode/src/test/java/edu/ahs/robotics/control/pid/PointPIDTest.java
@@ -0,0 +1,88 @@
+package edu.ahs.robotics.control.pid;
+
+import org.junit.Test;
+
+import edu.ahs.robotics.control.Point;
+
+import static org.junit.Assert.*;
+
+public class PointPIDTest {
+
+    @Test
+    public void testNull(){
+        Point p1 = new Point(1,1);
+        Point p2 = new Point(4,2);
+
+        PointPID pid = new PointPID(0,0,0);
+
+        PointPID.Correction correction = pid.getCorrection(p1,p2);
+
+        assertEquals(0,correction.x,0);
+        assertEquals(0,correction.y,0);
+    }
+
+    @Test
+    public void testSame(){
+        Point p = new Point(5,5);
+
+        PointPID pid = new PointPID(1,1,1);
+
+        PointPID.Correction correction = pid.getCorrection(p,p); // should be no correction when points are the same
+
+
+        assertEquals(0,correction.x,0);
+        assertEquals(0,correction.y,0);
+    }
+
+    @Test
+    public void testProportionality(){
+        Point p = new Point(1,1);
+        Point pClose = new Point(1.1,1.1);
+        Point pFar = new Point(2,2);
+
+        PointPID pid = new PointPID(1,0,0);
+
+        PointPID.Correction bigCorrection = pid.getCorrection(p,pFar);
+        PointPID.Correction smolCorrection = pid.getCorrection(p,pClose);
+
+        assertTrue(bigCorrection.x > smolCorrection.x);
+        assertTrue(bigCorrection.y > smolCorrection.y);
+
+        assertTrue(smolCorrection.x > 0); // should be greater than zero
+        assertTrue(smolCorrection.y > 0);
+    }
+
+    @Test
+    public void testIntegral(){
+        Point p1 = new Point(2,3);
+        Point p2 = new Point(6,6);
+
+        PointPID pid = new PointPID(0,1,0);
+
+        for(int i = 0; i < 4; i ++) {
+            pid.getCorrection(p1, p2); //wind up integral
+        }
+
+        PointPID.Correction correction = pid.getCorrection(p1,p2);
+
+        assertTrue(correction.x > 0);
+        assertTrue(correction.y > 0);
+    }
+
+    @Test
+    public void testDerivative(){
+        Point target = new Point(10,10);
+        Point far =  new Point(2,2);
+        Point close =  new Point(9,9);
+
+        PointPID pid = new PointPID(0,0,1);
+
+        pid.getCorrection(far,target);
+
+        PointPID.Correction correction = pid.getCorrection(close, target);
+
+        assertTrue(correction.x < 0); //derivative should be making a negative correction
+        assertTrue(correction.y < 0);
+    }
+
+}
