00a957df47b443c5dd5825a36468fdafdd1cae64
Appleby43 <alexappleby70@gmail.com>
2020-01-23 15:31:53 -0700

    Created new auto OpMode and disabled a bunch of old OpModes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RightCurveAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RightCurveAuto.java
@@ -30,6 +30,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -45,7 +46,7 @@ import edu.ahs.robotics.util.Logger;
 
 
 @Autonomous(name = "Right Curve Auto", group = "Linear Opmode")
-//@Disabled
+@Disabled
 public class RightCurveAuto extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StraightAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StraightAuto.java
@@ -30,6 +30,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -45,7 +46,7 @@ import edu.ahs.robotics.util.Logger;
 
 
 @Autonomous(name = "Straight Auto", group = "Linear Opmode")
-//@Disabled
+@Disabled
 public class StraightAuto extends LinearOpMode {
 
     @Override
