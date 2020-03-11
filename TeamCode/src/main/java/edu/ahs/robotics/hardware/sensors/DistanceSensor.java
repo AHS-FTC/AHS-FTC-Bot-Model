1e3c6b1b2dd615c84fbfbb39caa0873d1040a697
Appleby43 <alexappleby70@gmail.com>
2020-03-10 18:55:54 -0600

    Added and implemented getOptimizedDistance() resulting in ~70% better loop times. wow!

--- TeamCode/src/main/java/edu/ahs/robotics/hardware/sensors/DistanceSensor.java
+++ TeamCode/src/main/java/edu/ahs/robotics/hardware/sensors/DistanceSensor.java
@@ -7,9 +7,12 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import edu.ahs.robotics.util.ftc.FTCUtilities;
 
 public class DistanceSensor {
-    Rev2mDistanceSensor distanceSensor;
-    DistanceUnit distanceUnit;
-    //double toggleDistance;
+
+    private Rev2mDistanceSensor distanceSensor;
+    private DistanceUnit distanceUnit;
+    private long updateTime = 100;
+    private long lastTime;
+    private double previousDistance;
 
     public DistanceSensor(String sensorName) {
         this(sensorName, DistanceUnit.MM);
@@ -18,21 +21,29 @@ public class DistanceSensor {
     public DistanceSensor(String sensorName, DistanceUnit distanceUnit){
         distanceSensor = FTCUtilities.getDistanceSensor(sensorName);
         this.distanceUnit = distanceUnit;
+        lastTime = -updateTime; //ensures that first call to getDistOptimized queries
     }
-    //public void setToggleDistance(double toggleDistance){
-       // this.toggleDistance = toggleDistance;
-    //}
 
     public double getDist(){
         return distanceSensor.getDistance(distanceUnit);
     }
 
-//    public boolean isEngaged(){
-//        if (getDist()<=toggleDistance){
-//            return true;
-//        }else{
-//            return false;
-//        }
-//
-//    }
+    /**
+     * Gets distance only when time from previous sensor query exceeds updateTime. Otherwise returns previous query result.
+     */
+    public double getDistOptimized (){
+        long timeSinceLast = FTCUtilities.getCurrentTimeMillis() - lastTime;
+
+        if(timeSinceLast > updateTime){
+            previousDistance = getDist();
+            lastTime = FTCUtilities.getCurrentTimeMillis();
+        }
+
+        return previousDistance;
+    }
+
+    public void setUpdateTime(long updateTime){
+        this.updateTime = updateTime;
+    }
+
 }
